4a412099df5c42c467b99ff7211c6b2b39b107d1
Julio Sampaio <jsampaio@uolinc.com>
2018-10-22 19:30:29 -0300

    Add GH repository

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 
 if [ $GIT_IS_AVAILABLE -eq 0 ]; then
-  #git clone $HOME/.dotfiles
+  git clone https://github.com/juliosampaio/dotfiles.git $HOME/.dotfiles
   cd $HOME/.dotfiles
   echo "I need you to enter your sudo password so I can install some things:"
   sudo -v
