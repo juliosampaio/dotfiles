37912f368f3b53660fd77de2cd6eac161b7e3dab
Julio Sampaio <julio.sampaio@emma-sleep.com>
2024-07-07 01:20:07 +0100

    Add basic tmux config

--- .zshenv
+++ .zshenv
@@ -1,1 +1,2 @@
+#we leave this file in our home directory so that we can leave our main zshrc file in the .config directory
 export ZDOTDIR="~/.config/zshrc"
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
 	color_scheme = 'Dracula (Official)',
+	enable_tab_bar = false,
 	font_size = 16,
 	font = wezterm.font('JetBrains Mono'),	
 	window_background_image = os.getenv( "HOME" ) .. '/.config/assets/wezterm-background-image.gif',
@@ -9,4 +10,20 @@ return {
 		brightness = 0.1,
 	},
 	window_background_opacity = 0.9,
+	window_decorations = 'RESIZE',
+	keys = {
+		{
+			key = 'f',
+			mods = 'CTRL',
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
+	mouse_bindings = {
+	  -- Ctrl-click will open the link under the mouse cursor
+	  {
+	    event = { Up = { streak = 1, button = 'Left' } },
+	    mods = 'CTRL',
+	    action = wezterm.action.OpenLinkAtMouseCursor,
+	  },
+	},
 }
